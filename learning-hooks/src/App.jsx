da212964fbfc01ab8844cdcb37c38e9e23c3db0c
Aubrey Brusola <alexisaubrey.bonon.brusola@gmail.com>
2024-06-13 11:36:47 +0800

    Change the display from 'grid' to 'flex flex-col'

--- learning-hooks/src/App.jsx
+++ learning-hooks/src/App.jsx
@@ -2,7 +2,7 @@ import UseRef from "./UseRef/UseRef";
 
 function App() {
   return (
-    <div className="font-sans bg-blue-950 text-white text-lg h-dvh p-2.5">
+    <div className="font-sans bg-blue-950 text-white text-lg h-dvh p-2.5 flex flex-col gap-4">
       <UseRef />
     </div>
   );

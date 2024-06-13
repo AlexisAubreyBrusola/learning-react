c976499b094f4acaa18e1eac2662fd77d8dc2641
Aubrey Brusola <alexisaubrey.bonon.brusola@gmail.com>
2024-06-13 15:04:42 +0800

    Added the 'Timer' component

--- learning-hooks/src/UseRef/UseRef.jsx
+++ learning-hooks/src/UseRef/UseRef.jsx
@@ -1,15 +1,20 @@
 import InputFocus from "./InputFocus";
 import StoringPreviousValue from "./StoringPreviousValue";
+import Timer from "./Timer";
 
 const UseRef = () => {
   return (
     <>
-      <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
+      <div className="p-2.5 border-2 border-slate-400 rounded-md w-[310px] flex flex-col gap-2 h-fit">
         <InputFocus />
       </div>
 
-      <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
+      <div className="p-2.5 border-2 border-slate-400 rounded-md w-[310px] flex flex-col gap-2 h-fit">
         <StoringPreviousValue />
+      </div>
+
+      <div className="p-2.5 border-2 border-slate-400 rounded-md w-[310px] flex flex-col gap-2 h-fit">
+        <Timer />
       </div>
     </>
   );

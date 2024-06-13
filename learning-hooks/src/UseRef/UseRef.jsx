a7669a937694100aaa94a99a5fd10fae3dbcb4fe
Aubrey Brusola <alexisaubrey.bonon.brusola@gmail.com>
2024-06-13 11:21:19 +0800

    Added a 'h-fit' tailwindcss class to fix the height

--- learning-hooks/src/UseRef/UseRef.jsx
+++ learning-hooks/src/UseRef/UseRef.jsx
@@ -2,7 +2,7 @@ import InputFocus from "./InputFocus";
 
 const UseRef = () => {
   return (
-    <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2">
+    <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
       <InputFocus />
     </div>
   );

b80a8a460db077fb4f09afdef2527d663cdb3778
Aubrey Brusola <alexisaubrey.bonon.brusola@gmail.com>
2024-06-13 11:35:39 +0800

    Added 'StoringPreviousValue component'

--- learning-hooks/src/UseRef/UseRef.jsx
+++ learning-hooks/src/UseRef/UseRef.jsx
@@ -1,10 +1,17 @@
 import InputFocus from "./InputFocus";
+import StoringPreviousValue from "./StoringPreviousValue";
 
 const UseRef = () => {
   return (
-    <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
-      <InputFocus />
-    </div>
+    <>
+      <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
+        <InputFocus />
+      </div>
+
+      <div className="p-2.5 border-2 border-slate-400 rounded-md max-w-fit flex flex-col gap-2 h-fit">
+        <StoringPreviousValue />
+      </div>
+    </>
   );
 };
 
